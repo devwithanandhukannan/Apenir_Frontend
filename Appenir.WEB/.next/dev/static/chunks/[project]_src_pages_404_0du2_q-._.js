e05b12bdf46d480e7ac300b3eaacabc5214d2e2c
AbinJose2002 <abinjos307@gmail.com>
2026-07-10 03:36:25 +0530

    feat - initial admin dahsboard setup

--- Appenir.WEB/.next/dev/static/chunks/[project]_src_pages_404_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0bf8tmj._.js","static/chunks/[root-of-the-server]__00-185t._.js"],
-    source: "entry"
-});
